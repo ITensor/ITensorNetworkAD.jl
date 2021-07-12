25bb206b73c883f211bc0d15168d5e66930d2a7a
Matthew Fishman <mfishman@flatironinstitute.org>
2021-07-11 21:04:23 -0400

    Add more ChainRules rrules for ITensor operations

--- src/ITensorChainRules/ITensorChainRules.jl
+++ src/ITensorChainRules/ITensorChainRules.jl
@@ -3,10 +3,16 @@
 using ChainRulesCore
 using ITensors
 
+# Needed for defining the rule for `adjoint(A::ITensor)`
+# which currently doesn't work by overloading `ChainRulesCore.rrule`
+using ZygoteRules: @adjoint
+
 function ChainRulesCore.rrule(::typeof(getindex), x::ITensor, I...)
   y = getindex(x, I...)
   function getindex_pullback(ȳ)
-    x̄ = ITensor(inds(x))
+    # TODO: add definition `ITensor(::Tuple{}) = ITensor()`
+    # to ITensors.jl so no splatting is needed here.
+    x̄ = ITensor(inds(x)...)
     x̄[I...] = ȳ
     Ī = broadcast(_ -> NoTangent(), I)
     return (NoTangent(), x̄, Ī...)
@@ -14,14 +20,147 @@ function ChainRulesCore.rrule(::typeof(getindex), x::ITensor, I...)
   return y, getindex_pullback
 end
 
-function ChainRulesCore.rrule(::typeof(*), x1::ITensor, x2::ITensor)
+function setinds_pullback(ȳ, x, a...)
+  x̄ = ITensors.setinds(ȳ, inds(x))
+  ā = broadcast(_ -> NoTangent(), a)
+  return (NoTangent(), x̄, ā...)
+end
+
+for fname in (
+  :prime,
+  :setprime,
+  :noprime,
+  :replaceprime,
+  :swapprime,
+  :addtags,
+  :removetags,
+  :replacetags,
+  :settags,
+  :swaptags,
+  :replaceind,
+  :replaceinds,
+  :swapind,
+  :swapinds,
+)
+  @eval begin
+    function ChainRulesCore.rrule(::typeof($fname), x::ITensor, a...)
+      y = $fname(x, a...)
+      function f_pullback(ȳ)
+        return setinds_pullback(ȳ, x, a...)
+      end
+      return y, f_pullback
+    end
+  end
+end
+
+# TODO: This is not being called by Zygote for some reason,
+# using a Zygote overload directly instead. Figure out
+# why, maybe raise an issue.
+#function ChainRulesCore.rrule(::typeof(adjoint), x::ITensor)
+#  y = prime(x)
+#  function adjoint_pullback(ȳ)
+#    return setinds_pullback(ȳ, x)
+#  end
+#  return y, adjoint_pullback
+#end
+
+@adjoint function Base.adjoint(x::ITensor)
+  y = prime(x)
+  function setinds_pullback(ȳ)
+    x̄ = ITensors.setinds(ȳ, inds(x))
+    return (x̄,)
+  end
+  return y, setinds_pullback
+end
+
+function _contract_pullback(ȳ, x1, x2)
+  x̄1 = ȳ * x2
+  x̄2 = x1 * ȳ
+  return (NoTangent(), x̄1, x̄2)
+end
+
+function _rrule(::typeof(*), x1, x2)
   y = x1 * x2
   function contract_pullback(ȳ)
-    x̄1 = ȳ * x2
-    x̄2 = x1 * ȳ
-    return (NoTangent(), x̄1, x̄2)
+    return _contract_pullback(ȳ, x1, x2)
   end
   return y, contract_pullback
 end
+
+# Special case for contracting a pair of ITensors
+function ChainRulesCore.rrule(::typeof(*), x1::ITensor, x2::ITensor)
+  return _rrule(*, x1, x2)
+end
+
+function ChainRulesCore.rrule(::typeof(*), x1::Number, x2::ITensor)
+  return _rrule(*, x1, x2)
+end
+
+function ChainRulesCore.rrule(::typeof(*), x1::ITensor, x2::Number)
+  return _rrule(*, x1, x2)
+end
+
+function ChainRulesCore.rrule(::typeof(*), x1::ITensor, x2::ITensor, xs::ITensor...)
+  y = *(x1, x2, xs...)
+  function contract_pullback(ȳ)
+    # TODO: use some contraction sequence optimization here
+    tn = [x1, x2, xs...]
+    N = length(tn)
+    env_contracted = Vector{ITensor}(undef, N)
+    for n in 1:length(tn)
+      tn_left = tn[1:(n - 1)]
+      # TODO: define contract([]) = ITensor(1.0)
+      env_left = isempty(tn_left) ? ITensor(1.0) : contract(tn_left)
+      tn_right = tn[reverse((n + 1):end)]
+      env_right = isempty(tn_right) ? ITensor(1.0) : contract(tn_right)
+      env_contracted[n] = env_left * ȳ * env_right
+    end
+    return (NoTangent(), env_contracted...)
+  end
+  return y, contract_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(+), x1::ITensor, x2::ITensor)
+  y = x1 + x2
+  function add_pullback(ȳ)
+    return (NoTangent(), ȳ, ȳ)
+  end
+  return y, add_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(itensor), x::Array, a...)
+  y = itensor(x, a...)
+  function itensor_pullback(ȳ)
+    x̄ = array(ȳ)
+    ā = broadcast(_ -> NoTangent(), a)
+    return (NoTangent(), x̄, ā...)
+  end
+  return y, itensor_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(ITensor), x::Array, a...)
+  y = ITensor(x, a...)
+  function ITensor_pullback(ȳ)
+    # TODO: define `Array(::ITensor)` directly
+    x̄ = Array(ȳ, inds(ȳ)...)
+    ā = broadcast(_ -> NoTangent(), a)
+    return (NoTangent(), x̄, ā...)
+  end
+  return y, ITensor_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(ITensor), x::Number)
+  y = ITensor(x)
+  function ITensor_pullback(ȳ)
+    x̄ = ȳ[]
+    return (NoTangent(), x̄)
+  end
+  return y, ITensor_pullback
+end
+
+@non_differentiable Index(::Any...)
+@non_differentiable delta(::Any...)
+@non_differentiable dag(::Index)
+@non_differentiable inds(::Any...)
 
 end
--- test/ITensorChainRules/runtests.jl
+++ test/ITensorChainRules/runtests.jl
@@ -1,6 +1,8 @@
+using ChainRulesCore
 using ChainRulesTestUtils
 using FiniteDifferences
 using ITensors
+using ITensors.NDTensors
 using ITensorNetworkAD
 using Random
 using Test
@@ -8,7 +10,19 @@ using Zygote
 
 using Zygote: ZygoteRuleConfig
 
+#
+# ITensor extensions
+#
+
+# TODO: this is to fix an error within `ChainRulesTestUtils._test_add!!_behaviour`
+# that gets called in `test_rrule`. Is this needed/a good definition?
+Base.:+(x::Number, y::ITensor) = ITensor(x) + y
+Base.:+(x::ITensor, y::Number) = x + ITensor(y)
+
+#
 # For ITensor compatibility with FiniteDifferences
+#
+
 function FiniteDifferences.to_vec(A::ITensor)
   # TODO: generalize to sparse tensors
   return vec(array(A)), x -> itensor(x, inds(A))
@@ -18,27 +32,153 @@ function FiniteDifferences.rand_tangent(rng::AbstractRNG, A::ITensor)
   return randomITensor(eltype(A), inds(A))
 end
 
+function FiniteDifferences.rand_tangent(rng::AbstractRNG, A::ITensor)
+  # TODO: generalize to sparse tensors
+  return randomITensor(eltype(A), inds(A))
+end
+
+function FiniteDifferences.rand_tangent(rng::AbstractRNG, A::Tensor)
+  # TODO: generalize to sparse tensors
+  return randomTensor(eltype(A), inds(A))
+end
+
+function FiniteDifferences.rand_tangent(rng::AbstractRNG, x::Index)
+  return NoTangent()
+end
+
+#
 # For ITensor compatibility with ChainRulesTestUtils
+#
+
 function ChainRulesTestUtils.test_approx(
   actual::ITensor, expected::ITensor, msg=""; kwargs...
 )
   ChainRulesTestUtils.@test_msg msg isapprox(actual, expected; kwargs...)
 end
 
+function ChainRulesTestUtils.test_approx(
+  actual::ITensor, expected::Number, msg=""; kwargs...
+)
+  ChainRulesTestUtils.@test_msg msg isapprox(actual[], expected; kwargs...)
+end
+
+function ChainRulesTestUtils.test_approx(
+  actual::Number, expected::ITensor, msg=""; kwargs...
+)
+  ChainRulesTestUtils.@test_msg msg isapprox(actual, expected[]; kwargs...)
+end
+
 @testset "ITensorChainRules.jl" begin
   i = Index(2)
-  A = randomITensor(i', i)
+  A = randomITensor(i', dag(i))
+  B = randomITensor(i', dag(i))
 
   test_rrule(getindex, A, 1, 1; check_inferred=false)
   test_rrule(getindex, A, 1, 2; check_inferred=false)
   test_rrule(*, A', A; check_inferred=false)
+  test_rrule(*, 3.2, A; check_inferred=false)
+  test_rrule(*, A, 4.3; check_inferred=false)
+  test_rrule(+, A, B; check_inferred=false)
+  test_rrule(prime, A; check_inferred=false)
+  test_rrule(prime, A, 2; check_inferred=false)
+  test_rrule(addtags, A, "i"; check_inferred=false)
+  test_rrule(settags, A, "x,y"; check_inferred=false)
+  # XXX: broken with some ambiguity error in ChainRulesTestUtils
+  #test_rrule(delta, (i', i); check_inferred=false)
+  test_rrule(itensor, randn(2, 2), i', i; check_inferred=false)
+  test_rrule(ITensor, randn(2, 2), i', i; check_inferred=false)
+  test_rrule(ITensor, 2.3; check_inferred=false)
 
-  test_rrule(
-    ZygoteRuleConfig(),
-    (x, y) -> (x * y)[1, 1],
-    A',
-    A;
-    rrule_f=rrule_via_ad,
-    check_inferred=false,
-  )
+  f = adjoint
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = (x, y) -> (x * y)[1, 1]
+  args = (A', A)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> prime(x, 2)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> x'[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> addtags(x, "x")[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x' * x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (prime(x) * x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> ((x'' * x') * x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x'' * (x' * x))[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = (x, y, z) -> (x * y * z)[1, 1]
+  args = (A'', A', A)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x'' * x' * x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x''' * x'' * x' * x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x''' * x'' * x' * x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = (x, y) -> (x + y)[1, 1]
+  args = (A, B)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x + x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (2x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x + 2x)[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x + 2 * mapprime(x' * x, 2 => 1))[1, 1]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = (x, y) -> (x * y)[]
+  args = (A, δ(dag(inds(A))))
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x * x)[]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x * δ(dag(inds(x))))[]
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = function (x)
+    y = x' * x
+    tr = δ(dag(inds(y)))
+    return (y * tr)[]
+  end
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = function (x)
+    y = x'' * x' * x
+    tr = δ(dag(inds(y)))
+    return (y * tr)[]
+  end
+  args = (A,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x^2 * δ((i', i)))[1, 1]
+  args = (2.2,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> (x^2 * δ(i', i))[1, 1]
+  args = (2.2,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> itensor([x^2 x; x^3 x^4], i', i)
+  args = (2.3,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> ITensor([x^2 x; x^3 x^4], i', i)
+  args = (2.3,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
+  f = x -> ITensor(x)
+  args = (2.3,)
+  test_rrule(ZygoteRuleConfig(), f, args..., rrule_f=rrule_via_ad, check_inferred=false)
 end
