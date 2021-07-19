0dbe95be6ddbfbd7c561e5dd00e0b4e84bd4b775
Matthew Fishman <mfishman@flatironinstitute.org>
2021-07-19 14:30:43 -0400

    Format

--- test/ITensorNetworks/itensor_network.jl
+++ test/ITensorNetworks/itensor_network.jl
@@ -17,7 +17,15 @@ using ITensorNetworkAD.ITensorNetworks: itensor_network, neighbors
     @test hascommoninds(tn[1, 1], tn[3, 1])
     @test hascommoninds(tn[1, 1], tn[1, 3])
     @test isempty(uniqueinds(tn[2, 2], tn[2, 1], tn[2, 3], tn[1, 2], tn[3, 2]))
-    @test issetequal(neighbors(tn, CartesianIndex(1, 2)), [CartesianIndex(1, 1), CartesianIndex(2, 2), CartesianIndex(3, 2), CartesianIndex(1, 3)])
+    @test issetequal(
+      neighbors(tn, CartesianIndex(1, 2)),
+      [
+        CartesianIndex(1, 1),
+        CartesianIndex(2, 2),
+        CartesianIndex(3, 2),
+        CartesianIndex(1, 3),
+      ],
+    )
   end
   @testset "itensor_network from siteinds" begin
     d = (3, 3)
@@ -32,6 +40,14 @@ using ITensorNetworkAD.ITensorNetworks: itensor_network, neighbors
     @test hascommoninds(tn[1, 1], tn[3, 1])
     @test hascommoninds(tn[1, 1], tn[1, 3])
     @test uniqueinds(tn[2, 2], tn[2, 1], tn[2, 3], tn[1, 2], tn[3, 2]) == [s[2, 2]]
-    @test issetequal(neighbors(tn, CartesianIndex(1, 2)), [CartesianIndex(1, 1), CartesianIndex(2, 2), CartesianIndex(3, 2), CartesianIndex(1, 3)])
+    @test issetequal(
+      neighbors(tn, CartesianIndex(1, 2)),
+      [
+        CartesianIndex(1, 1),
+        CartesianIndex(2, 2),
+        CartesianIndex(3, 2),
+        CartesianIndex(1, 3),
+      ],
+    )
   end
 end
