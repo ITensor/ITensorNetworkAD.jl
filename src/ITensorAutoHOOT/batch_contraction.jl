dd5f3d37dc9da6bef986df29422aef5029b73449
LinjianMa <malinjianev@gmail.com>
2021-07-28 20:15:59 -0500

    Add NetworkCache with constrained contraction path optimization

--- src/ITensorAutoHOOT/batch_contraction.jl
+++ src/ITensorAutoHOOT/batch_contraction.jl
@@ -55,6 +55,26 @@ end
 
 @non_differentiable NetworkCache(networks::Array)
 
+"""
+Generate a cached network, under the constraint that tensors in contract_order will be
+contracted based on the order from Array start to the end.
+"""
+# NOTE: this function is experimental and it hasn't been used in experiments yet.
+function NetworkCache(networks::Array, contract_order::Array{<:ITensor})
+  nodes, node_dict = generate_einsum_expr(networks)
+  constrained_nodes = [retrieve_key(node_dict, t) for t in contract_order]
+  function contraction_path(node)
+    constrained_inputs = [n for n in constrained_nodes if n in node.inputs]
+    out = go.generate_optimal_tree_w_constraint(node, constrained_inputs)
+    return out
+  end
+  net_sums = [NetworkSum([contraction_path(n)]) for n in nodes]
+  node_index_dict = generate_node_index_dict(node_dict, networks)
+  return NetworkCache(net_sums, node_index_dict)
+end
+
+@non_differentiable NetworkCache(networks::Array, contract_order::Array{<:ITensor})
+
 # TODO: add caching intermediates here
 function run(net_sum::NetworkSum, feed_dict::Dict)
   return ITensors.sum(compute_graph(net_sum.nodes, feed_dict))
