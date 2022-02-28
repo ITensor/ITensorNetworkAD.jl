753014dcea2e2b5350df63c853ae8688ab2ad964
LinjianMa <malinjianev@gmail.com>
2022-02-27 22:20:00 -0600

    Remove ITensorChainRules

--- src/ITensorNetworkAD.jl
+++ src/ITensorNetworkAD.jl
@@ -3,7 +3,6 @@ module ITensorNetworkAD
 using Reexport
 
 include("Profiler/Profiler.jl")
-include("ITensorChainRules/ITensorChainRules.jl")
 include("ITensorAutoHOOT/ITensorAutoHOOT.jl")
 include("ITensorNetworks/ITensorNetworks.jl")
 include("Optimizations/Optimizations.jl")
